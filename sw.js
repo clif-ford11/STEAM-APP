f6c8f6d7bc90aa6fd6456cf68c9f20fd7d5726e6
clif-ford11 <luyunallenclifford21@gmail.com>
2026-02-05 15:16:34 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "my-pwa-cache-v1";
+const urlsToCache = [
+  "/steam-app/",
+  "/steam-app/index.html",
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
